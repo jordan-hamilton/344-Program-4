f8efbdfe106bbeed79328cfffc85adcaaf926bca
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2019-12-07 18:34:11 -0700

    Update templates for decryption

--- otp_dec.c
+++ otp_dec.c
@@ -1,72 +1,196 @@
+#include <ctype.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-// Error function used for reporting issues
-void error(const char *msg) {
-  perror(msg);
-  exit(0);
-}
+void error(const char* msg);
+void fileToBuffer(const int*, char[], const int*);
+void receiveStringFromSocket(const int*, char[], char[], const int*, const char[]);
+void sendStringToSocket(const int*, const char[]);
+void validateString(const char[]);
 
 int main(int argc, char *argv[]) {
-  int socketFD, portNumber, charsWritten, charsRead;
+  int socketFD, portNumber;
+  int plaintextFD, plaintextLength, keyFD, keyLength;
   struct sockaddr_in serverAddress;
   struct hostent* serverHostInfo;
-  char buffer[256];
+  int bufferSize = 100000, messageFragmentSize = 10;
+  char buffer[bufferSize], messageFragment[messageFragmentSize];
+  char connectionValidator[] = ">>";
+  char endOfMessage[] = "||";
 
   // Check usage & args
-  if (argc < 3) {
-    fprintf(stderr,"USAGE: %s hostname port\n", argv[0]);
-    exit(0);
+  if (argc < 4) {
+    fprintf(stderr, "Correct command format: %s PLAINTEXT KEY PORT\n", argv[0]);
+    exit(1);
   }
 
   // Set up the server address struct
-  memset((char*)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
-  portNumber = atoi(argv[2]); // Get the port number, convert to an integer from a string
+  memset((char*) &serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
+  portNumber = atoi(argv[3]); // Get the port number, convert to an integer from a string
   serverAddress.sin_family = AF_INET; // Create a network-capable socket
   serverAddress.sin_port = htons(portNumber); // Store the port number
-  serverHostInfo = gethostbyname(argv[1]); // Convert the machine name into a special form of address
+  serverHostInfo = gethostbyname("localhost"); // Convert the machine name into a special form of address
+
   if (serverHostInfo == NULL) {
-    fprintf(stderr, "CLIENT: ERROR, no such host\n");
+    fprintf(stderr, "An error occurred defining a server address.\n");
     exit(0);
   }
-  memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length); // Copy in the address
+  memcpy((char*) &serverAddress.sin_addr.s_addr, (char*) serverHostInfo->h_addr, serverHostInfo->h_length); // Copy in the address
 
   // Set up the socket
   socketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
   if (socketFD < 0)
-    error("CLIENT: ERROR opening socket");
+    error("An error occurred creating a socket");
 
   // Connect to server
-  if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to address
-    error("CLIENT: ERROR connecting");
-
-  // Get input message from user
-  printf("CLIENT: Enter text to send to the server, and then hit enter: ");
-  memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer array
-  fgets(buffer, sizeof(buffer) - 1, stdin); // Get input from the user, trunc to buffer - 1 chars, leaving \0
-  buffer[strcspn(buffer, "\n")] = '\0'; // Remove the trailing \n that fgets adds
+  if (connect(socketFD, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) < 0) // Connect socket to address
+    error("An error occurred connecting to the server");
 
   // Send message to server
-  charsWritten = send(socketFD, buffer, strlen(buffer), 0); // Write to the server
-  if (charsWritten < 0)
-    error("CLIENT: ERROR writing to socket");
-  if (charsWritten < strlen(buffer))
-    printf("CLIENT: WARNING: Not all data written to socket!\n");
+  sendStringToSocket(&socketFD, ">>||");
 
   // Get return message from server
-  memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer again for reuse
-  charsRead = recv(socketFD, buffer, sizeof(buffer) - 1, 0); // Read data from the socket, leaving \0 at end
+  memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer
+  receiveStringFromSocket(&socketFD, buffer, messageFragment, &messageFragmentSize, endOfMessage);
+  /*charsRead = recv(socketFD, buffer, sizeof(buffer) - 1, 0); // Read data from the socket, leaving \0 at end
   if (charsRead < 0)
-    error("CLIENT: ERROR reading from socket");
-  printf("CLIENT: I received this from the server: \"%s\"\n", buffer);
+    error("An error occurred reading from the socket");*/
+
+  if (strcmp(buffer, connectionValidator) != 0) {
+    fprintf(stderr, "A connection was made to an unknown destination.\n");
+    exit(1);
+  } else {
+
+    /* Open the specified plaintext and key files, checking for existence and setting the length of each file in
+     * bytes so we can verify the key we'll send to the daemon is long enough to encrypt the plaintext message. */
+    plaintextFD = open(argv[1], O_RDONLY);
+    if (plaintextFD < 0)
+      error("Could not open the specified plaintext file");
+    /* Set the length of the provided plaintext equal to the size of the file
+     * (source: https://stackoverflow.com/questions/174531/how-to-read-the-content-of-a-file-to-a-string-in-c). */
+    plaintextLength = lseek(plaintextFD, 0, SEEK_END);
+
+    // Repeat the above steps for our key to get its size.
+    keyFD = open(argv[2], O_RDONLY);
+    if (keyFD < 0)
+      error("Could not open the specified key file");
+    keyLength = lseek(keyFD, 0, SEEK_END);
+
+    // Print an error message and exit if the key is too short to use.
+    if (keyLength < plaintextLength) {
+      fprintf(stderr, "The provided key does not meet the minimum length requirements to "
+                      "encrypt your message.\nPlease provide a key with a length of %d or more.\n", plaintextLength - 1);
+      exit(1);
+    }
+
+    memset(buffer, '\0', sizeof(buffer));
+    fileToBuffer(&plaintextFD, buffer, &plaintextLength);
+    close(plaintextFD);
+    validateString(buffer);
+    sendStringToSocket(&socketFD, buffer);
+
+    memset(buffer, '\0', sizeof(buffer));
+    fileToBuffer(&keyFD, buffer, &keyLength);
+    close(keyFD);
+    validateString(buffer);
+    sendStringToSocket(&socketFD, buffer);
+
+    sendStringToSocket(&socketFD, endOfMessage);
+
+    // Get return message from server
+    memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer again for reuse
+    receiveStringFromSocket(&socketFD, buffer, messageFragment, &messageFragmentSize, endOfMessage);
+    /*
+    charsRead = recv(socketFD, buffer, sizeof(buffer) - 1, 0); // Read data from the socket, leaving \0 at end
+    if (charsRead < 0)
+      error("An error occurred reading from the socket");*/
+    fprintf(stdout, "%s\n", buffer);
+  }
 
   close(socketFD); // Close the socket
 
   return(0);
+}
+
+// Error function used for reporting issues
+void error(const char* msg) {
+  perror(msg);
+  exit(0);
+}
+
+/* Takes a file descriptor pointer, a buffer to store the file's contents and a pointer to the length of the file,
+ * then uses the read function to store the provided number of bytes from the file into the buffer. */
+void fileToBuffer(const int* fileDescriptor, char buffer[], const int* fileLength) {
+  int bytesRead = -5;
+  lseek(*fileDescriptor, 0, SEEK_SET);
+  bytesRead = read(*fileDescriptor, buffer, *fileLength);
+  if (bytesRead < 0)
+    error("An error occurred trying to read file contents");
+}
+
+void receiveStringFromSocket(const int* establishedConnectionFD, char message[], char messageFragment[], const int* messageFragmentSize, const char endOfMessage[]) {
+  int charsRead = -5;
+  long terminalLocation = -5;
+
+  while (strstr(message, endOfMessage) == NULL) {
+    memset(messageFragment, '\0', *messageFragmentSize);
+    charsRead = recv(*establishedConnectionFD, messageFragment, *messageFragmentSize - 1, 0);
+
+    if (charsRead == 0)
+      break;
+    if (charsRead == -1)
+      break;
+
+    strcat(message, messageFragment);
+  }
+
+  /* Find the terminal location using the method in the Network Clients video from Block 4
+   * then set a null terminator after the actual message contents end. */
+  terminalLocation = strstr(message, endOfMessage) - message;
+  message[terminalLocation] = '\0';
+}
+
+/* Takes a pointer to a socket, followed by a string to send via that socket,
+ * then loops to ensure all the data in the string is sent. */
+void sendStringToSocket(const int* socketFD, const char message[]) {
+  int charsWritten;
+  // Send message to server
+  charsWritten = send(*socketFD, message, strlen(message), 0); // Write to the server
+  if (charsWritten < 0)
+    error("An error occurred writing to the socket");
+
+  while (charsWritten < strlen(message)) {
+    int addedChars = 0;
+    // Write to the server again, starting from one character after the most recently sent character
+    addedChars = send(*socketFD, message + charsWritten, strlen(message) - charsWritten, 0);
+    if (addedChars < 0)
+      error("An error occurred writing to the socket");
+
+    // Exit the loop if no more characters are being sent to the server.
+    if (addedChars == 0) {
+      break;
+    }
+
+    // Add the number of characters written in an iteration to the total number of characters sent in the message
+    charsWritten += addedChars;
+  }
+}
+
+/* Takes a string then uses a loop starting from the beginning of the string to check one character at a time,
+ * ensuring that the character is either a space or uppercase letter. Exits the loop at the end of the string
+ * or exits the program when an invalid character is found that can't be sent to our daemon. */
+void validateString(const char buffer[]) {
+  for (int i = 0; i < strlen(buffer); i++) {
+    if (!isupper(buffer[i]) && !isspace(buffer[i])) {
+      fprintf(stderr, "One or more invalid characters were detected.\n");
+      exit(1);
+    }
+  }
 }
